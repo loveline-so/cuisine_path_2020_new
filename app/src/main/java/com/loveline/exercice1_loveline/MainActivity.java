6502446008972eb627b39d23cbceb0d1acab610c
loveline-so <lovelinesokou@mail.com>
2020-06-01 00:03:20 +0100

    Exercice1_loveline

---
+++ app/src/main/java/com/loveline/exercice1_loveline/MainActivity.java
@@ -0,0 +1,14 @@
+package com.loveline.exercice1_loveline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
