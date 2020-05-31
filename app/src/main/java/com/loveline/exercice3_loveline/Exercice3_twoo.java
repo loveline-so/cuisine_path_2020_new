9e4b9c977fe847a30fb4b9bef6f0f71bcbda072f
loveline-so <lovelinesokou@mail.com>
2020-06-01 00:15:10 +0100

    Exercice3_loveline

---
+++ app/src/main/java/com/loveline/exercice3_loveline/Exercice3_twoo.java
@@ -0,0 +1,14 @@
+package com.loveline.exercice3_loveline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Exercice3_twoo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercice3_twoo);
+    }
+}
