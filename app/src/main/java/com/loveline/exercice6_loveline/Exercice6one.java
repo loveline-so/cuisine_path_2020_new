f6a33d7e99baeb57e9e8b93c9135f92d1844ad64
loveline-so <lovelinesokou@mail.com>
2020-06-01 00:34:45 +0100

    Exercice6_loveline

---
+++ app/src/main/java/com/loveline/exercice6_loveline/Exercice6one.java
@@ -0,0 +1,14 @@
+package com.loveline.exercice6_loveline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Exercice6one extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercice6one);
+    }
+}
---
+++ app/src/main/java/com/loveline/exercice6_loveline/Exercice6two.java
@@ -0,0 +1,14 @@
+package com.loveline.exercice6_loveline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Exercice6two extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercice6two);
+    }
+}
